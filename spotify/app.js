b017d8083064b7199a58fd4537a1e6e7d5a6ebc6
Apsara Bishwokarma <apsarabishwokarma7@gmail.com>
2022-06-11 09:26:39 +0545

    changed

--- spotify/app.js
+++ spotify/app.js
@@ -17,12 +17,12 @@ let songs= [
     {songName: "moonlight-aliGatie", filePath: "songs/ali.mp3", coverPath: "covers/Ali1.jpg"},
     {songName: "sasto mutu-sajjan Raj vaidya", filePath: "songs/sastomutu.mp3", coverPath: "covers/srv4.jpg"},
     {songName: "najik na aau-bartika rai", filePath: "songs/najik.mp3", coverPath: "covers/najik.jpg"},
-    {songName: "hataridai batasidai-sajjan raj vaidya", filePath: "songs/hataarindai.mp3", coverPath: "covers/srv2.jpg"},
-    {songName: "hawaijahaj batasidai-sajjan raj vaidya", filePath: "songs/Hawaijahaj.mp3", coverPath: "covers/srv1.jpg"},
-    {songName: "lukamari-sajjan Raj vaidya", filePath: "songs/lukamari.mp3", coverPath: "covers/srv3.jpg"},
-    {songName: "khamosiyaan-Arjit singh", filePath: "songs/khamoshiyan.mp3", coverPath: "covers/kha1.jpg"},
+    {songName: "hataridai batasidai-sajjan raj vaidya", filePath:"songs/hataarindai.mp3", coverPath: "covers/srv2.jpg"},
+    {songName: "hawaijahaj batasidai-sajjan raj vaidya", filePath:"songs/Hawaijahaj.mp3", coverPath: "covers/srv1.jpg"},
+    {songName: "lukamari-sajjan Raj vaidya", filePath:"songs/lukamari.mp3", coverPath: "covers/srv3.jpg"},
+    {songName: "khamosiyaan-Arjit singh", filePath:"songs/khamoshiyan.mp3", coverPath: "covers/kha1.jpg"},
 ]
-  
+
 
 songItems.forEach((element, i)=>{ 
     element.getElementsByTagName("img")[0].src = songs[i].coverPath; 
@@ -72,7 +72,7 @@ Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
         songIndex = parseInt(e.target.id);
         e.target.classList.remove('fa-circle-play');
         e.target.classList.add('fa-circle-pause');
-        audioElement.src ='songs/{songIndex+1}.mp3';//'songs/${songIndex+ali}.mp3' ;//to update songs accordingly
+        audioElement.src = songs[songIndex].filePath;//to update songs accordingly
         masterSongName.innerText = songs[songIndex].songName;
         audioElement.currentTime = 0;
         audioElement.play();
@@ -90,7 +90,7 @@ document.getElementById('next').addEventListener('click', ()=>{
     else{
         songIndex += 1;
     }
-    audioElement.src = 'songs/${songIndex+1}.mp3';
+ audioElement.src = songs[songIndex].filePath;
     masterSongName.innerText = songs[songIndex].songName;
     audioElement.currentTime = 0;
     audioElement.play();
@@ -106,7 +106,7 @@ document.getElementById('previous').addEventListener('click', ()=>{
     else{
         songIndex -= 1;
     }
-    audioElement.src = 'songs/${songIndex+1}.mp3';//
+    audioElement.src = songs[songIndex].filePath;
     masterSongName.innerText = songs[songIndex].songName;
     audioElement.currentTime = 0;
     audioElement.play();

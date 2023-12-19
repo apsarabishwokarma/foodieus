3f33f7e509cd49d68f7cd0b8467863dcc30c15e0
apsarabiswokarma <apsarabk94@gmail.com>
2023-12-19 18:12:01 +0545

    checked todos fixed

--- TodoList/script.js
+++ TodoList/script.js
@@ -63,8 +63,8 @@ todoForm.addEventListener("submit", function (event) {
     title: title,
     date: date,
     completed: false,
-    id: `todotask${todos.length + 1}`,
-    class: `todotask${todos.length + 1}`,
+    id: `todotask${todos.length + 1}${date}`,
+    class: `todotask${todos.length + 1}${date}`,
   };
   todos.push(newTodo);
   document.getElementById("todoInput").value = "";
@@ -78,18 +78,40 @@ function renderTodos() {
 
     tasksContainer.innerHTML += `
       <div class="todoDetails">
-      <input type="checkbox" id="${todo.id}" ${
-      todo?.completed ? "checked" : ""
-    }/>
-      <label for="${todo.id}" class="${todo.class}">
-        <div class="todoDate">${todo.date}</div>
-        <p class="task">${todo.title}</p>
-      </label>
-      <i class="fa-solid fa-trash" onclick="remove('${todo.id}')"></i>
+        <input type="checkbox" id="${todo.id}" ${
+        todo?.completed ? "checked" : ""
+      } onchange="toggleComplete(this)"/>
+
+        <label for="${todo.id}" class="${todo.class}">
+          <div class="todoDate">${todo.date}</div>
+          <p class="task">${todo.title}</p>
+        </label>
+        
+        <i class="fa-solid fa-trash" onclick="remove('${todo.id}')"></i>
     </div>
     `;
   }
   localStorage.setItem("todos", JSON.stringify(todos)); //object to string
+}
+
+function toggleComplete(target) {
+  console.log(target);
+  const id = target.id;
+
+  todos = todos.map((t) => {
+    if (id === t.id) {
+      const newTodo = {
+        ...t,
+        completed: target.checked,
+      };
+
+      return newTodo;
+    } else {
+      return t;
+    }
+  });
+
+  renderTodos();
 }
 
 function remove(idToRemove) {
@@ -100,7 +122,7 @@ function remove(idToRemove) {
 addEventListener("DOMContentLoaded", (event) => {
   const localTodos = localStorage.getItem("todos");
   if (localTodos) {
-    //not undefined
+    //not equal to  undefined
     todos = JSON.parse(localTodos); //string to object
     renderTodos();
   }
